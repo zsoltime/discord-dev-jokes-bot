cd78898d6cb001a05d7da5ad35c889705eabf2cb
Zsolt Meszaros <zsolt@sowad.com>
2019-10-06 18:31:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# 🤖 Discord Dev Jokes Bot 🤖
+
+> Discord bot that posts dad style programming jokes.
+
+## Tools Used
+
+- [Discord.js](https://www.npmjs.com/package/discord.js) to interact with the Discord API easily
+- [Dad style programming jokes](https://github.com/wesbos/dad-jokes)
+- [ESLint](https://github.com/eslint/eslint) linter with Airbnb's [base config](https://www.npmjs.com/package/eslint-config-airbnb-base)
+- [Jest](https://github.com/facebook/jest) test framework
+
+## Install and Build
+
+#### Clone this repo
+
+```bash
+git clone https://github.com/zsoltime/discord-dev-jokes-bot.git
+cd discord-dev-jokes-bot
+```
+
+#### Install dependencies
+
+```bash
+npm install
+```
+
+#### Start dev server
+
+It starts a dev server, monitor for changes and restarts on any change.
+
+```bash
+npm run dev
+```
+
+#### Start
+
+It starts the node.js application.
+
+```bash
+npm start
+```
+
+#### Run tests
+
+It runs tests using Jest.
+
+```bash
+npm test
+```
+
+## Credits
+
+Bot icons are made by <a href="https://www.flaticon.com/authors/freepik">Freepik</a> from <a href="https://www.flaticon.com/">Flaticon</a>
---
+++ src/index.js
@@ -0,0 +1,73 @@
+const { Client, RichEmbed } = require('discord.js');
+const random = require('utils.random');
+
+const jokes = require('./jokes.json');
+
+const { TOKEN } = process.env;
+const bot = new Client();
+
+function getEmbedJoke() {
+  const avatar = random([
+    'https://i.imgur.com/JuzuXKS.png',
+    'https://i.imgur.com/QIKp17x.png',
+    'https://i.imgur.com/zCesLaS.png',
+    'https://i.imgur.com/AzAAr7R.png',
+  ]);
+  const color = 0x03a3f3;
+  const joke = random(jokes);
+  const title = 'This one will make you smile 😅';
+
+  return new RichEmbed()
+    .setColor(color)
+    .setDescription(joke)
+    .setThumbnail(avatar)
+    .setTitle(title);
+}
+
+function handleIncomingMessage(message) {
+  const msg = message.content.toLowerCase();
+
+  if (!message.isMentioned(bot.user)) {
+    if (msg === '!joke') {
+      message.channel.send(getEmbedJoke());
+    }
+    return;
+  }
+
+  if (msg.includes('joke')) {
+    message.channel.send(
+      `Do you want to hear a joke, ${message.author}?`
+    );
+    message.channel.send(getEmbedJoke());
+    return;
+  }
+
+  if (msg.includes('good bot')) {
+    message.channel.send(
+      `Aww, ${message.author}, I'm blushing... 😊`
+    );
+    return;
+  }
+
+  if (msg.includes('source') || msg.includes('code')) {
+    message.channel.send(
+      "I'm a really dumb bot, but if you wanted to see my source code, you can visit my master's GitHub repo: https://github.com/zsoltime/discord-dev-jokes-bot 😉"
+    );
+    return;
+  }
+
+  message.channel.send('Did someone menion me?');
+  message.channel.send(
+    "If you want me to tell a joke, just say so. Don't forget to mention my name though 😉"
+  );
+}
+
+// eslint-disable-next-line no-console
+bot.on('ready', () => console.log('Bot is connected...'));
+bot.on('message', handleIncomingMessage);
+bot.login(TOKEN);
+
+module.exports = {
+  getEmbedJoke,
+  handleIncomingMessage,
+};
